69f02dbd3cf96ef44d4b713430a7e889e08495fa
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2021-11-23 21:05:44 +0000

    Update install_custom_fonts.sh

--- src/ubuntu/install/fonts/install_custom_fonts.sh
+++ src/ubuntu/install/fonts/install_custom_fonts.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Installing ttf-wqy-zenhei"
 if [ "$DISTRO" = centos ]; then
-  wget https://centos.pkgs.org/7/centos-aarch64/wqy-zenhei-fonts-0.9.46-11.el7.noarch.rpm.html
+  wget http://mirror.centos.org/altarch/7/os/aarch64/Packages/wqy-zenhei-fonts-0.9.46-11.el7.noarch.rpm
   yum localinstall -y wqy-zenhei-fonts*.rpm
   rm wqy-zenhei-fonts*.rpm
 else
